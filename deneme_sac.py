a68735fac53cce5f2e7c4fc197c03b3110a1a6d6
xxaralin <nil.lara.tan@hotmail.com>
2021-08-24 17:05:56 +0300

    sacdenemeler

--- deneme_sac.py
+++ deneme_sac.py
@@ -11,9 +11,11 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Normal
 from tensorboardX import SummaryWriter
-hidden_dim=200
-use_cuda = torch.cuda.is_available()
-device   = torch.device("cuda" if use_cuda else "cpu")
+import datcom_gym_env
+
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+
 class ValueNetwork(nn.Module):
     def __init__(self, state_dim, hidden_dim, init_w=3e-3):
         super(ValueNetwork, self).__init__()
@@ -78,9 +80,37 @@ class PolicyNetwork(nn.Module):
         
         log_std = self.log_std_linear(x)
         log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
-        return mean, log_std
-
-    def evaluate(self,state,epsilon=1e-6):
+        
+        std = torch.exp(log_std)
+        
+        log_prob = None
+        
+        if deterministic:
+            action = torch.tanh(mean)
+        else:
+            # assumes actions have been normalized to (0,1)
+            normal = Normal(0, 1)
+            z = mean + std * normal.sample().requires_grad_()
+            action = torch.tanh(z)
+            log_prob = Normal(mean, std).log_prob(z) - torch.log(1 - action * action + self.epsilon)
+            
+        return action, mean, log_std, log_prob, std
+    
+    def get_action(self, state, deterministic=False):
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
+        action,_,_,_,_ =  self.forward(state, deterministic)
+        act = action.cpu()[0][0]
+        return act
+    """
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        
+        mean    = self.mean_linear(x)
+        
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        
         mean,log_std=self.forward(state)
         std =log_std.exp()
 
@@ -90,49 +120,38 @@ class PolicyNetwork(nn.Module):
         log_prob = Normal(mean, std).log_prob(z) - torch.log(1 - action * action + self.epsilon)
         return action, mean, log_std, log_prob, std
     
-    def get_action(self, state, deterministic=False):
-        state = torch.FloatTensor(state).unsqueeze(0).to(device)
-        action,_,_,_,_ =  self.forward(state, deterministic)
+    def get_action(self, state):
+        state = torch.FloatTensor(state.reshape(1,-1)).to(device)
+        action,_,_,_ =  self.forward(state)
         act = action.cpu()[0][0]
-        return act
+        return act"""
         
-class ReplayBuffer:
-    def __init__(self, max_size=1000):
+class ReplayBuffer(object):
+    def __init__(self,max_size=1000):
+        self.storage=[]
         self.max_size=max_size
-        self.buffer = []
-        self.position = 0
-    
-    def push(self, state, action, reward, next_state, done):
-        if len(self.buffer) < self.max_size:
-            self.buffer.append(None)
-        
-        self.buffer[self.position] = (state, action, reward, next_state, done)
-        self.position = (self.position + 1) % self.max_size
-    
-    def sample(self, batch_size):
-        batch = random.sample(self.buffer, batch_size)
-        state, action, reward, next_state, done = map(np.stack, zip(*batch))
-        return state, action, reward, next_state, done
-    
-    def __len__(self):
-        return len(self.buffer)
-        
-class NormalizedActions(gym.ActionWrapper):
-    def action(self, action):
-        low  = self.action_space.low
-        high = self.action_space.high
+        self.ptr=0
 
-        action = low + (action + 1.0) * 0.5 * (high - low)
-        action = np.clip(action, low, high)
-        
-        return action
+    def add(self,data):#experience replay tuple-agents experience at a time t(yine sars)
+        if len(self.storage)==self.max_size:
+            self.storage[int(self.ptr)]=data
+            self.ptr=(self.ptr+1)% self.max_size
+            #storage dolduysa baştan tekrar doldurmaya başlıyor i guess?
+        else:
+            self.storage.append(data)
+    def sample(self,batch_size):#train edeceği batch size
+        ind=np.random.randint(0,len(self.storage),size=batch_size) #random number within the range
+        states,actions,next_states,rewards,dones=[],[],[],[],[]
+        for i in ind:
+            s,a,s_,r,d=self.storage[i]
+            states.append(np.array(s,copy=False))
+            actions.append(np.array(a,copy=False))
+            next_states.append(np.array(s_,copy=False))
+            rewards.append(np.array(r,copy=False))
+            dones.append(np.array(d,copy=False))
+        return np.array(states),np.array(actions),np.array(next_states),np.array(rewards).reshape(-1,1),np.array(dones).reshape(-1,1)
+  
 
-    def normalize_action(action, low, high):
-        action = low + (action + 1.0) * 0.5 * (high - low)
-        action = np.clip(action, low, high)
-        
-        return action
-    
 class SAC(object):
     
     def __init__(self, env, replay_buffer, seed=0, hidden_dim=200,
@@ -142,6 +161,7 @@ class SAC(object):
         
         # Set seeds
         self.env = env
+        obs=env.reset()
         self.env.seed(seed)
         torch.manual_seed(seed)
         np.random.seed(seed)
@@ -152,7 +172,8 @@ class SAC(object):
         self.hidden_dim = hidden_dim
         
         # device
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        #self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cpu")
         
         # init networks
         
@@ -192,15 +213,23 @@ class SAC(object):
         self.batch_size = batch_size
         self.tau = tau
         
-    def get_action(self, state, deterministic=False, explore=False):
+        """  
+    def select_action(self, state):#pretty self explanatory
+        state=torch.FloatTensor(state.reshape(1,-1)).to(device)
+
+        action=self.agent(state)
+        action=action.cpu().data.numpy().flatten()
+        return action.clip(self.env.action_space.low, self.env.action_space.high)
+        """  
+    def get_action(self, state, explore=False):
         
         state = torch.FloatTensor(state).unsqueeze(0).to(device)
         if explore:
             return self.env.action_space.sample()
         else:
-            action  = self.policy_net.get_action(state, deterministic).detach()
+            action  = self.policy_net.get_action(state).detach()
             return action.numpy()
-           
+        
     def update(self, iterations, batch_size = 100):
         
         for _ in range(0,iterations):
@@ -213,7 +242,7 @@ class SAC(object):
             reward     = torch.FloatTensor(reward).unsqueeze(1).to(device)
             done       = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
 
-            new_actions, policy_mean, policy_log_std, log_pi, *_ = self.policy_net.evaluate(state)
+            new_actions, policy_mean, policy_log_std, log_pi, *_ = self.policy_net(state)
 
             if self.auto_alpha:
                 alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
@@ -271,3 +300,67 @@ class SAC(object):
                 target_param.data.copy_(
                     target_param.data * (1.0 - self.tau) + param.data * self.tau
                 )
+def train(agent, steps_per_epoch=1000, epochs=100, start_steps=1000, max_ep_len=200):
+    
+    # start tracking time
+    start_time = time.time()
+    total_rewards = []
+    avg_reward = None
+    
+    obs, r, d, ep_reward, ep_len, ep_num = env.reset(), 0, False, 0, 0, 1
+    
+    # track total steps
+    total_steps = steps_per_epoch * epochs
+    
+    for t in range(1,total_steps):
+        
+        explore = t < start_steps
+        a = agent.get_action(obs)
+        new_obs, r, d, _=env.step(a,obs)
+        normed_state=(agent.obs-agent.state_lower)/(agent.state_upper-agent.state_lower)
+        
+###        # Step the env
+        new_obs, r, d, _ = env.step(a,normed_state)
+        ep_reward += r
+        ep_len += 1
+
+        d = False if ep_len == max_ep_len else d
+        replay_buffer.add(obs, a, r, new_obs, d)
+        obs = new_obs
+        
+        if d or (ep_len == max_ep_len):
+        
+            # carry out update for each step experienced (episode length)
+            if not explore:
+                agent.update(ep_len)
+            
+            # log progress
+            total_rewards.append(ep_reward)
+            avg_reward = np.mean(total_rewards[-100:])
+            
+            print("Steps:{} Episode:{} Reward:{} Avg Reward:{}".format(t, ep_num, ep_reward, avg_reward))
+#             logger.store(LossPi=outs[0], LossQ1=outs[1], LossQ2=outs[2],
+#                          LossV=outs[3], Q1Vals=outs[4], Q2Vals=outs[5],
+#                          VVals=outs[6], LogPi=outs[7])
+
+#             logger.store(EpRet=ep_ret, EpLen=ep_len)
+            obs, r, d, ep_reward, ep_len = env.reset(), 0, False, 0, 0
+            ep_num += 1
+
+        # End of epoch wrap-up
+        if t > 0 and t % steps_per_epoch == 0:
+            epoch = t // steps_per_epoch
+
+            # TODO: Save Model
+
+            # TODO: Test
+
+            # TODO: Log Epoch Results
+
+replay_buffer = ReplayBuffer()
+
+env = gym.make("Datcom-v1")
+hidden_dim=200
+agent = SAC(env, replay_buffer)
+
+train(agent)
